a0928e96ccdcc84c84a7efb49ba2d596039f8eb9
git-dru <mastercryto.us@gmail.com>
2023-12-12 06:48:17 +0200

    npm reinstall into client folder for card draggable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,7 @@
-import { useEffect, useState } from "react";
 import FlightBoard from "./components/FlightBoard";
 import './App.css';
 
 export const App = () => {
-	const [apiStatus, setApiStatus] = useState<string | null>(null);
-
-	useEffect(() => {
-		fetch("/api/health")
-			.then((response) => response.json())
-			.then((data) => {
-				setApiStatus(data.status);
-			});
-	}, []);
-
 	return (
 		<div>
 			<FlightBoard />
